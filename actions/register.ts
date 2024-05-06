634e86ded4bbf38b14c88e59820ef5bdba2412b1
Akbar Sayakov <38359586+akbarsayakov@users.noreply.github.com>
2024-05-06 15:45:26 -0700

    create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,9 @@
 'use server'
 
+import { db } from '@/lib/db'
 import { RegisterSchema } from '@/schemas'
 import * as z from 'zod'
+import brcypt from 'bcrypt'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values)
@@ -9,5 +11,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validateFields.success) {
     return { error: 'Invalid fields!' }
   }
-  return { success: 'Email sent!' }
+
+  const { email, password, name } = validateFields.data
+
+  const existingEmail = await db.user.findUnique({ where: { email } })
+
+  if (existingEmail) {
+    return { error: 'Email already exists' }
+  }
+
+  const hashedPassword = await brcypt.hash(password, 10)
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  })
+
+  return { success: 'User created!' }
 }
